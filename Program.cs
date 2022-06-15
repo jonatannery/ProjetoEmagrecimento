e11add939d60e14cb85699e317222105a67d7eab
jonatan nery <jonatanoliveiranery@gmail.com>
2022-06-15 11:08:28 -0300

    pegando os dados do usuário

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace ProgramaEmagrecimento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Início do projeto
+
+            //variaveis do projeto
+            int idade;
+            double peso, altura, IMC;
+            string nome, sexo, categoria;
+
+            Console.WriteLine("Estamos dando o início ao   para o programa de " +
+                "emagrecimento, por favor insira seus dados.");
         }
     }
 }
