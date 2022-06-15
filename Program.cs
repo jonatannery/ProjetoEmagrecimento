43ca6eddd77fe1247fb0049d2f06d6e6fe54ee86
jonatan nery <jonatanoliveiranery@gmail.com>
2022-06-15 10:01:50 -0300

    início do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramaEmagrecimento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
