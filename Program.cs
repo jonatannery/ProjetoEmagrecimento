3e034536aea978b81552128e9d1538794090fd6b
jonatan nery <jonatanoliveiranery@gmail.com>
2022-06-15 13:35:31 -0300

    código da classificação IMC e a idade criados

--- Program.cs
+++ Program.cs
@@ -11,10 +11,43 @@ namespace ProgramaEmagrecimento
             //variaveis do projeto
             int idade;
             double peso, altura, IMC;
-            string nome, sexo, categoria;
-
+            string nome  , sexo, categoria;
+            
+            // O usuário esta tendo a infomação do inicio do programa.
             Console.WriteLine("Estamos dando o início ao   para o programa de " +
-                "emagrecimento, por favor insira seus dados.");
+                "emagrecimento");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("por favor, insira seus dados.");
+
+            Console.WriteLine("");
+            // Obtendo o nome do usuário
+             
+            Console.Write("\nNome : ");
+            nome = Console.ReadLine();
+            
+            //Obtendo o sexo do usuário            
+            Console.Write("\nSexo : " );
+            sexo = Console.ReadLine();
+
+            //Obetendo a idade do usuário           
+            Console.Write("\nIdade : ");
+            idade = int.Parse(Console.ReadLine());
+
+            //Obtendo a altura do usuário
+            Console.Write("\nAltura : ");
+            altura = double.Parse(Console.ReadLine());
+
+            //Obtendo o peso do usuário
+            Console.Write("\nPeso: ");
+            peso=double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Nome:{nome}\n"+
+                              $"idade:{idade}\n"+
+                              $"sexo:{altura}\n");
+
+
         }
     }
 }
