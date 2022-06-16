d359b0d919a06866a6554c921bb6d5069b2a3bcf
jonatan nery <jonatanoliveiranery@gmail.com>
2022-06-15 22:30:48 -0300

    foi criado a formula IMC  e agora vai estar iniciando os dados da classificação.

--- Program.cs
+++ Program.cs
@@ -10,38 +10,40 @@ namespace ProgramaEmagrecimento
             // Início do projeto
 
             //variaveis do projeto
-            int idade=0;
-            double peso, altura, IMC;
+            int idade = 0;
+            double peso, altura;
             string nome, sexo, categoria;
-            bool validarIdade , validadeAltura, validadePeso;
+            bool validarIdade, validadeAltura, validadePeso;
 
             // O usuário esta tendo a infomação do inicio do programa.
-            Console.WriteLine("Sistema de controle de IMC. ");
+            Console.WriteLine("Sistema de controle de IMC. \n");
 
 
-            Console.WriteLine("");
+
 
             Console.WriteLine("por favor, insira seus dados. \n");
 
-           
-            // Obtendo o nome do usuário
+
+            // Obtendo o nome do usuário.
 
             Console.Write("Nome : ");
             nome = Console.ReadLine();
 
-            //Obtendo o sexo do usuário            
+            //Obtendo o sexo do usuário.       
             Console.Write("Sexo (M ou F): ");
-            sexo = Console.ReadLine().ToUpper(); 
+            sexo = Console.ReadLine().ToUpper(); //ToUpper vai deixar a primeira letra da palavra maiuscula .
 
-            //atribuindo o caractere para o sexo do usuário
-
+            //atribuindo o caractere para o sexo do usuário "m" para masculino e "f " para feminino com uma condição
+            //caso o usuário digite palavras que nao seja "m" ou "f".
             while (sexo != "M" && sexo != "F")
             {
-
+                //se o usuário digitar palavras diferente do que é pedido ,aparecerá essa informação
                 Console.WriteLine("informação invalida, insira o sexo novamente.");
+
+                // repetindo para o usuario digitar novamente ,caso seja diferente do que é pedido.
                 Console.Write("Sexo (M ou F): ");
                 sexo = Console.ReadLine().ToUpper();
-                
+
             }
             //atribuindo o sexo.
             if (sexo == "M")
@@ -54,51 +56,78 @@ namespace ProgramaEmagrecimento
             }
 
 
-            //Obetendo a idade do usuário           
+            //Obtendo a idade do usuário           
             Console.Write("Idade : ");
-            validarIdade = int.TryParse(Console.ReadLine(),out idade);
+            validarIdade = int.TryParse(Console.ReadLine(), out idade);
 
-            while (validarIdade == false ||idade <=0)  // idade tem que ser valores positivos
+            //validando somente números positivos.
+            while (validarIdade == false || idade <= 0)  // idade tem que ser valores positivos
             {
                 Console.WriteLine("Não identificamos sua idade .Por favor insira a idade novamente:");
                 Console.Write("Idade : ");
-                validarIdade = int.TryParse(Console.ReadLine(),out idade);
+                validarIdade = int.TryParse(Console.ReadLine(), out idade);
             }
 
 
             //Obtendo a altura do usuário
             Console.Write("Altura : ");
-            validadeAltura = double.TryParse(Console.ReadLine(),out altura);
+            validadeAltura = double.TryParse(Console.ReadLine(), out altura);
             while (validadeAltura == false || altura <= 0)
             {
                 //altura deve ter valores positivos
+                //validando somente números positivos.
                 Console.WriteLine("Não identificamos sua altura.Por favor insira a altura novamente: ");
                 Console.Write("Altura : ");
                 validadeAltura = double.TryParse(Console.ReadLine(), out altura);
             }
 
+
             //obtendo o peso do usuário
             Console.Write("Peso : ");
-            validadePeso =double.TryParse(Console.ReadLine(),out peso);
-            while(validadePeso == false || peso <= 0)
+            validadePeso = double.TryParse(Console.ReadLine(), out peso);
+            while (validadePeso == false || peso <= 0)
             {
                 //peso deve ter valores positivos
+                //validando somente números positivos.
                 Console.WriteLine("Não identificamos seu peso. Por favor insira o peso novamente!");
                 Console.Write("Peso : ");
                 validadePeso = double.TryParse(Console.ReadLine(), out peso);
 
             }
 
-            // saber  a categoria do usuário
-            Console.Write("Categoria : ");
-            int entrarCategoria = idade;
-            categoria=Categoria(entrarCategoria);
+            // saber  a categoria do usuário.
 
+            /* foi criado uma variavel int "auxCategoria" recebendo a "idade" ,
+            a variavel "auxCategoria" foi colocada dentro da função
+            "Categoria:aonde mostra a categoria do usuário " e a variavel "Categoria" recebeu
+            a função "Categoria(auxCategoria).*/
+            Console.Write("Categoria : ");
+            int auxCategoria = idade;
+            categoria = Categoria(auxCategoria);
             Console.WriteLine(categoria);
 
-        }
+            // foi criado o método "Imc" obtendo a fórmula para calcular o IMC do usuário.
+            Console.WriteLine("IMC desejável: entre 20 e 24 .");
+            Console.Write("Resultado IMC: ");
+            double auxImc = Imc(altura, peso);
+            Console.WriteLine(auxImc);
 
-        private static string Categoria(int idade)
+
+
+            /*foi criado o metodo " Classificação" para visualizar a classificação do usuario 
+            com os dados de "riscos" e " recomendações iniciais "*/
+
+
+
+
+
+
+
+
+        }
+        // condição pra obter a categoria do usuário
+        //foi criado um método para saber a categoria do usuário 
+        static string Categoria(int idade)
         {
             if (idade <= 12)
             {
@@ -118,7 +147,10 @@ namespace ProgramaEmagrecimento
             }
 
         }
-        private void Classificacao(double imc)
+
+        //Foi criado um método para saber a classificação do usuario
+        //A classificação é baseada no índice de massa corporal(IMC).
+        static double Classificacao(double imc)
         {
             if (imc < 20)
             {
@@ -127,7 +159,7 @@ namespace ProgramaEmagrecimento
                 Console.WriteLine(@"Riscos: Muitas complicações de saúde como doenças pulmonares 
                                    e cardiovasculares podem estar associadas ao baixo peso. \t");
 
-                Console.WriteLine(@"Inclua carboidratos simples em sua dieta, além de proteínas -
+                Console.WriteLine(@"Recomendação inicial:Inclua carboidratos simples em sua dieta, além de proteínas -
                                     indispensáveis para ganho de massa magra. Procure um profissional .");
             }
             else if (imc >= 20 && imc <= 24)
@@ -172,11 +204,15 @@ namespace ProgramaEmagrecimento
 
             }
 
-
+            return imc;
 
         }
 
-
+        static double Imc(double altura, double peso)
+        {
+            double imc = peso / Math.Pow(altura, 2);
+            return imc;
+        }
 
     }
 
